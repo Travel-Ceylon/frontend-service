e385d3ade0cca4351bdde7a980943c9bed4c8db8
nishmikaeka <nurannishmika22@gmail.com>
2025-12-12 13:07:41 +0530

    Fixed the authstore functionality so every domain can use that not only taxi:fix the issue

--- src/store/authStore.js
+++ src/store/authStore.js
@@ -5,48 +5,56 @@ import toast from "react-hot-toast";
 export const useAuthStore = create((set) => ({
   user: null,
   isAuthChecking: false,
+  setUser: (user) => set({ user }),
 
   // LOGIN
   login: async (credentials) => {
     set({ loading: true, error: null });
     try {
-      const { data } = await api.post("/service-provider/login", credentials);
+      const { data } = await api.post(
+        "/api/service-provider/login",
+        credentials
+      );
       if (data) {
         set({ user: data, loading: false });
+        return data.data;
       } else {
         set({ user: null, loading: false });
+        return null;
       }
     } catch (err) {
-      toast.error(err.response?.data?.message)
-      set({ user: null});
+      toast.error(err.response?.data?.message);
+      set({ user: null });
     }
   },
 
   // REGISTER
   register: async (userData) => {
     try {
-      const { data } = await api.post("/service-provider", userData);
+      const { data } = await api.post("/api/service-provider", userData);
       console.log(data);
 
       if (data) {
         set({ user: data, loading: false });
+        return data.data;
       } else {
         set({ user: null, loading: false });
       }
-
     } catch (err) {
-      set({ user: null});
+      toast.error(err.response?.data?.message);
+      set({ user: null });
     }
   },
 
   // LOGOUT
   logout: async () => {
     try {
-      const { data } = await api.post("/service-provider/logout");
-      console.log(data)
+      const { data } = await api.post("/api/service-provider/logout");
+      console.log(data);
       set({ user: null, loading: false });
     } catch (err) {
-      set({ user: null});
+      toast.error(err.response?.data?.message);
+      set({ user: null });
     }
   },
 
@@ -54,17 +62,17 @@ export const useAuthStore = create((set) => ({
   loadUser: async () => {
     set({ isAuthChecking: true });
     try {
-      const { data } = await api.get("/service-provider/me");
+      const { data } = await api.get("/api/service-provider/me");
       if (data) {
         set({ user: data, loading: false });
       } else {
         set({ user: null, loading: false });
       }
-
     } catch (err) {
-      return null
+      toast.error(err.response?.data?.message);
+      return null;
     } finally {
-      set({ isAuthChecking: false })
+      set({ isAuthChecking: false });
     }
   },
 
@@ -72,16 +80,16 @@ export const useAuthStore = create((set) => ({
   updateProfile: async (updates) => {
     set({ loading: true, error: null });
     try {
-      const { data } = await api.put("/service-provider/me", updates);
+      const { data } = await api.put("/api/service-provider/me", updates);
       if (data) {
         set({ user: data, loading: false });
-        toast.success("Updated successfully!")
+        toast.success("Updated successfully!");
       } else {
         set({ user: null, loading: false });
       }
     } catch (err) {
-      set({user: null});
+      toast.error(err.response?.data?.message);
+      set({ user: null });
     }
-  }
-
+  },
 }));
